30086eb72be4283fb833019a3ad13ce27280aaa9
Connor Linfoot <connorlinfoot@me.com>
2016-03-28 06:45:03 +0100

    Added more game modes and method to get nice name

--- src/main/java/com/connorlinfoot/mctools/Hypixel/GameMode.java
+++ src/main/java/com/connorlinfoot/mctools/Hypixel/GameMode.java
@@ -1,5 +1,35 @@
 package com.connorlinfoot.mctools.Hypixel;
 
 public enum GameMode {
-	SKYWARS, BLITZ_SG, CRAZY_WALLS, UNKNOWN
+	SKYWARS,
+	BLITZ_SG,
+	CRAZY_WALLS,
+	COPS_AND_CRIMS,
+	THE_WALLS,
+	MEGA_WALLS,
+	UHC_CHAMPIONS,
+	UNKNOWN;
+
+	public String getNiceName() {
+		switch(this) {
+			case SKYWARS:
+				return "SkyWars";
+			case BLITZ_SG:
+				return "Survival Games";
+			case CRAZY_WALLS:
+				return "Crazy Walls";
+			case COPS_AND_CRIMS:
+				return "Cops and Crims";
+			case THE_WALLS:
+				return "The Walls";
+			case MEGA_WALLS:
+				return "Mega Walls";
+			case UHC_CHAMPIONS:
+				return "UHC Champions";
+			case UNKNOWN:
+				return "Unknown";
+		}
+		return this.toString();
+	}
+
 }
