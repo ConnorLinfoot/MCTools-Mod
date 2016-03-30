c0a9d2d6d153a943e8504992edbfb957c0e2fd59
Connor Linfoot <connorlinfoot@me.com>
2016-03-30 11:25:13 +0100

    Change version to 0.1 for beta!

--- src/main/java/com/connorlinfoot/mctools/MCTools.java
+++ src/main/java/com/connorlinfoot/mctools/MCTools.java
@@ -35,7 +35,7 @@ public class MCTools {
 	private static MCTools mcTools;
 	public static final String prefix = TextFormatting.GRAY + "[" + TextFormatting.GOLD + "MC Tools" + TextFormatting.GRAY + "] " + TextFormatting.RESET;
 	public static final String MODID = "mctools";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.1";
 	private ConfigHandler configHandler;
 	private Hypixel hypixel;
 	public static KeyBinding quickActions;
